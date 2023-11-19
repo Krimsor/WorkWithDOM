d9efae68b4e73fafc72ee348bd782e04d6081698
Krimsor <v_suffering@mail.ru>
2023-11-19 14:22:15 +1000

    HomeWork001

--- HomeWork001/HW001/script.js
+++ HomeWork001/HW001/script.js
@@ -1,3 +1,8 @@
+/* <div id="block">
+	<p>1</p>
+	<p>2</p>
+</div> */
+
 // Получите ссылку на первый абзац из дива с id, равным block, выведите его в консоль
 // Получите ссылку на первый абзац с классом www. и вывести его в консоль
 // <p class="www">text 1</p>
@@ -23,4 +28,57 @@
 // imageEl[0].src = 'https://vean-tattoo.com/media/images/007_8tbf2SD.original.format-jpeg.jpg'
 
 
+// Дан тег <div class="content"></div> 
+// Создайте новый элемент p
+// Добавьте в него текст “Новый текстовый элемент”
+// Добавьте созданный элемент внутри <div class="content"></div>
+// Удалите добавленный узел 
 
+// const paragraphEl = document.createElement('p');
+// paragraphEl.innerHTML = 'Новый текстовый элемент';
+
+// const divEl = document.getElementsByClassName('content')[0];
+
+// const button = document.createElement('button');
+// button.innerHTML = 'Click me';
+// let counter = 0;
+
+// divEl.appendChild(button);
+
+// button.onclick = function() {
+//     counter++;
+//     console.log(counter);
+// };
+
+// divEl[0].appendChild(paragraphEl);
+
+// paragraphEl.remove();
+
+
+// Дан тег <div class="content"></div> 
+// Создайте с помощью javascript новый элемент button
+// Добавьте текст для кнопки “Отправить”
+// При клике на данную кнопку необходимо чтобы текст поменялся на “Текст отправлен”
+
+// const divEl = document.getElementsByClassName('content')[0];
+
+// const button = document.createElement('button');
+// button.innerHTML = 'Отправить';
+
+// divEl.appendChild(button);
+
+// button.onclick = function() {
+//     button.innerHTML = 'Текст отправлен'    
+// };
+
+// const arr = ['Лист 1', 'Лист 2', 'Лист 3'];
+
+// const ulElement = document.createElement('ul');
+
+// for (let item in arr) {
+//     let liElement = document.createElement('li');
+//     liElement.innerHTML = arr[item];
+//     ulElement.appendChild(liElement);
+// }
+
+// divEl.appendChild(ulElement);
