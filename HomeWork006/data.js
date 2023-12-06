0b025eccdfe87c16d6715bfbe32edab34e98d1b4
Krimsor <v_suffering@mail.ru>
2023-12-07 01:16:42 +1000

    HomeWork006

---
+++ HomeWork006/data.js
@@ -0,0 +1,18 @@
+let dataProductsJSON = `[
+    {
+        "image" : "1.jpg",
+        "title" : "MANGO  PEOPLE  T-SHIRT",
+        "price" : "$300",
+        "color" : "Red",
+        "size" : "Xl",
+        "quantity" : "2"
+    },
+    {
+        "image" : "2.jpg",
+        "title" : "ELLERY X M'O CAPSULE",
+        "price" : "$52",
+        "color" : "Black",
+        "size" : "Xl",
+        "quantity" : "1"
+    }
+]`;
